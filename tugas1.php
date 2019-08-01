653c15e3f34991ec64fa9d6715c967ff75173bf1
Silvana <silvanafadilla15@gmail.com>
2019-08-01 17:05:09 +0700

    menambahakan dua file baru

---
+++ tugas1.php
@@ -0,0 +1,8 @@
+<?php
+    $harga_kg = 15000;
+    $kg = $harga_kg*30;
+    $perdus = 6*2000;
+    $jml = $kg+$perdus;
+
+    echo "Hasil penjualan seluruh jambu dan dus adalah : Rp. $jml";
+?>
---
+++ tugas2.php
@@ -0,0 +1,17 @@
+<?php
+    $tama = 1;
+    // git push -u origin master --force
+    $toni = 0,5;
+    $tiniTama = 14;
+    $tiniToni = 34;
+
+    echo "Umur Tama = $tamaX<br>";
+    echo "Umur Toni = $toniX<br>";
+    echo "Umur Toni + Umur Tini = $tiniToni<br>";
+    echo "$toni + $tamaX - $tiniTama = $tiniToni<br>";
+    $x = $tama + $toni;
+    $y = $tiniTama + $tiniToni;
+    $z = $y / $x;
+    echo "$x X = $tiniTama + $tiniToni";
+    
+?>
